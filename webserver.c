25413b54b9188e4b3bc54cbbf9cc5b2e7dd74bda
SiddheshDhinge <saddy97531@gmail.com>
2024-03-31 16:17:57 +0530

    fixed small bugs and formatted the code

--- webserver.c
+++ webserver.c
@@ -11,6 +11,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+const int PORT = 3005;
 const int sizeOfRequest = 1024;
 
 char* getPath(unsigned char* buff)
@@ -34,12 +35,14 @@ char* getPath(unsigned char* buff)
 
     int sizeOfPath = endIndex - startIndex;
 
-    char* requestedPath = (char*) malloc(sizeof(char) * sizeOfPath);
+    char* requestedPath = (char*) malloc(sizeof(char) * sizeOfPath + 1);
 
     for(int i=startIndex; i<endIndex; i++)
     {
         requestedPath[i - startIndex] = buff[i];
     }
+
+    requestedPath[sizeOfPath] = '\0';
 
     return requestedPath;
 }
@@ -70,7 +73,6 @@ int size(char *ptr)
     return offset;
 }
 
-
 int main()
 {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -78,40 +80,43 @@ int main()
     // create a server socket FD 
     if(sockfd < 0)
     {
-        printf("Cant create socket");
+        printf("Cant create socket3\n");
         return -1;
     }
 
     // create socket configs
     address.sin_family = AF_INET;
-    address.sin_port = htons(3005);
+    address.sin_port = htons(PORT);
     address.sin_addr.s_addr = INADDR_ANY;
 
-    int enable;
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
+    int enable = 1;
+    if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
+    {
+        printf("Cant set SO_REUSEADDR option.\n");
+        return -1;
+    }
 
     // bind to socket
     int retVal = bind(sockfd, (struct sockaddr *) &address, sizeof(address));
     if(retVal < 0)
     {
-        printf("Cant bind to socket");
+        printf("Cant bind to socket\n");
         return -1;
     }
 
     // listen on socket
-    retVal = listen(sockfd, 1024);
+    retVal = listen(sockfd, 4096);
 
     if(retVal < 0)
     {
-        printf("Cant listen to socket");
+        printf("Cant listen to socket\n");
         return -1;
     }
 
-    printf("Server is working at http://localhost:3005 🔥🔥🔥\n");
+    printf("Server is listening at http://localhost:%d 🔥🔥🔥\n\n", PORT);
 
     while(1)
     {
-        
         // client connects
         struct sockaddr_in clientaddress;
         int sizeOfClientAddress = 0;
@@ -119,36 +124,37 @@ int main()
         
         if(clientfd < 0)
         {
-            printf("Cant accept on socket");
+            printf("Cant accept on socket\n");
             return -1;
         }
 
         // read what client says
         unsigned char *buff = (unsigned char*) malloc(sizeof(unsigned char) * sizeOfRequest);
+        memset(buff, '\0', sizeOfRequest); 
+        
         int bytesRead = read(clientfd, buff, sizeOfRequest);
 
-        printf("START REQ\n");
+        printf("============= START REQ =============\n");
         fflush(stdout);
         
-        printf("%s", buff);
+        printf("%s\n", buff);
 
-        printf("END REQ\n");
+        printf("============= END REQ ==============\n");
 
         int pathSize = 0;
-        char* path = getPath(buff);
+        char* path = getPath(buff); 
         pathSize = size(path);
 
         printf("Path Size: %d\n", pathSize);
-        printf("PATH: ");
-        puts(path);
+        printf("PATH: %s\n", path);
         
         int fileNameSize = 0;
         char* fileName = getFileForPath(path, pathSize);
         fileNameSize = size(fileName);
         
         printf("File Name Size: %d\n", fileNameSize);
-        printf("File Name: ");
-        puts(fileName);
+        printf("File Name: %s\n", fileName);
+        
         fflush(stdout);
 
         if(fileNameSize == 0)
@@ -169,9 +175,8 @@ int main()
             goto CLOSE;
         }
 
-        printf("Sending Reponse...");
+        printf("Sending Reponse...\n");
         char* responseBuffer = (char*) malloc(sizeof(char) * 1024);
-
         read(resFileFD, responseBuffer, 1024);
 
         char responseWithHeader[2048] = "HTTP/1.1 200 OK Error\r\nContent-Type: text/plain\r\n\r\n ";
@@ -181,10 +186,16 @@ int main()
 
         write(clientfd, responseWithHeader, actualResSize);
 
+        free((void *) responseBuffer);
+
     CLOSE:
-        close(clientfd);
+        free((void *) buff);
+        free((void *) path);
         
-        printf("Response sent 👍\n");
+        close(clientfd);
+
+        printf("Response sent 👍\n\n\n");
+        fflush(stdout);
     }
 
     close(sockfd);
